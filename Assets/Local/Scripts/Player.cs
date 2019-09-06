17a12b35eec7e302620074ccbf9c9b8176f0e281
Pedro Weber <pedroweberm@gmail.com>
2019-09-06 15:05:13 -0300

    shader seethrough e comeco da movimentacao

---
+++ Assets/Local/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movement;
+    Rigidbody playerRigidBody;
+    float camRayLenght = 100f;
+    int floorMask;
+
+    private void Awake()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+        floorMask = LayerMask.GetMask("Floor");
+    }
+
+    public void Move(Vector3 move)
+    {
+        if (move.magnitude > 1f) move.Normalize();
+
+        movement = move * speed * Time.deltaTime;
+
+        Debug.Log(movement);
+
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+
+    public void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLenght, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidBody.MoveRotation(newRotation);
+        }
+    }
+}
---
+++ Assets/Local/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class PlayerController : MonoBehaviour
+{
+    
+    private Player m_Player;                  // A reference to the Player on the object
+    private Vector3 m_Move;                   // the world-relative desired move direction, calculated from the camForward and user input.
+
+
+    private void Awake()
+    {
+        m_Player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        m_Move.Set(h, 0f, v);
+
+        m_Player.Turning();
+
+        m_Player.Move(m_Move);
+    }
+
+    
+}
