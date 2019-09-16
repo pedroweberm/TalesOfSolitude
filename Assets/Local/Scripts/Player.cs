b551efe2848e94f9c02187a89976db3491471b99
Pedro Weber <pedroweberm@gmail.com>
2019-09-16 19:36:46 -0300

    movimentacao por clique (ainda bugado)

--- Assets/Local/Scripts/Player.cs
+++ Assets/Local/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
@@ -10,11 +11,74 @@ public class Player : MonoBehaviour
     Rigidbody playerRigidBody;
     float camRayLenght = 100f;
     int floorMask;
+    NavMeshAgent playerAgent;
+    Animator m_Animator;
+    float m_TurnAmount;
+    float m_ForwardAmount;
+    Vector2 smoothDeltaPosition = Vector2.zero;
+    Vector2 velocity = Vector2.zero;
+
 
     private void Awake()
     {
         playerRigidBody = GetComponent<Rigidbody>();
         floorMask = LayerMask.GetMask("Floor");
+        playerAgent = GetComponent<NavMeshAgent>();
+        m_Animator = GetComponent<Animator>();
+        playerAgent.updatePosition = false;
+    }
+
+    public void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(clickRay, out hitInfo, camRayLenght, floorMask))
+            {
+                playerAgent.SetDestination(hitInfo.point);
+            }
+        }
+
+        Vector3 worldDeltaPosition = playerAgent.nextPosition - transform.position;
+
+        // Map 'worldDeltaPosition' to local space
+        float dx = Vector3.Dot(transform.right, worldDeltaPosition);
+        float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
+        Vector2 deltaPosition = new Vector2(dx, dy);
+
+        // Low-pass filter the deltaMove
+        float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+        smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, deltaPosition, smooth);
+
+        // Update velocity if time advances
+        if (Time.deltaTime > 1e-5f)
+            velocity = smoothDeltaPosition / Time.deltaTime;
+
+        m_TurnAmount = Mathf.Atan2(velocity.x, velocity.y);
+        m_ForwardAmount = velocity.y;
+
+        Debug.Log(velocity);
+
+        UpdateAnimator();
+    }
+
+    void UpdateAnimator()
+    {
+        // update the animator parameters
+        m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
+        m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
+        m_Animator.SetBool("Crouch", false);
+        m_Animator.SetBool("OnGround", true);
+        m_Animator.SetFloat("Jump", 0);
+        m_Animator.SetFloat("JumpLeg", 0);
+    }
+
+    void OnAnimatorMove()
+    {
+        // Update position to agent position
+        transform.position = playerAgent.nextPosition;
     }
 
     public void Move(Vector3 move)
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.AI;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
@@ -12,10 +13,16 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+        private int floorMask;
+        bool mooving = false;
+        float camRayLenght = 100f;
+        NavMeshAgent playerAgent;
 
-        
+
         private void Start()
         {
+            floorMask = LayerMask.GetMask("Floor");
+            playerAgent = GetComponent<NavMeshAgent>();
             // get the transform of the main camera
             if (Camera.main != null)
             {
@@ -38,38 +45,38 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             if (!m_Jump)
             {
                 m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-            }
+            } 
         }
 
 
         // Fixed update is called in sync with physics
         private void FixedUpdate()
         {
-            // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitInfo;
 
-            // calculate move direction to pass to character
-            if (m_Cam != null)
-            {
-                // calculate camera relative direction to move:
-                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
+                if (Physics.Raycast(clickRay, out hitInfo, camRayLenght, floorMask))
+                {
+                    playerAgent.SetDestination(hitInfo.point);
+                }
             }
-            else
+
+            if (!playerAgent.pathPending)
             {
-                // we use world-relative directions in the case of no main camera
-                m_Move = v*Vector3.forward + h*Vector3.right;
+                if (playerAgent.remainingDistance <= playerAgent.stoppingDistance)
+                {
+                    if (!playerAgent.hasPath || playerAgent.velocity.sqrMagnitude == 0f)
+                    {
+                        m_Character.Move(Vector3.zero, false, m_Jump);
+                        Debug.Log("Chegou ja");
+                    }
+                }
             }
-#if !MOBILE_INPUT
-			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
-#endif
 
             // pass all parameters to the character control script
-            m_Character.Move(m_Move, crouch, m_Jump);
-            m_Jump = false;
+            m_Character.Move(playerAgent.destination - transform.position, false, m_Jump);
         }
     }
 }
