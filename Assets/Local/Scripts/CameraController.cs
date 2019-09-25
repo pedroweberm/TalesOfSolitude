8532d09c01aee5c3ea3b9913cdc19d02ab053db4
Pedro Weber <pedroweberm@gmail.com>
2019-09-25 15:16:07 -0300

    ajustes no posicionamento da camera, escala dos objetos e personagem e na movimentacao (nao eh mais possivel caminhar na agua ou nas beiradas)

--- Assets/Local/Scripts/CameraController.cs
+++ Assets/Local/Scripts/CameraController.cs
@@ -13,6 +13,8 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        transform.position = new Vector3(200, 70, target.position.z - 45);
+
         offset = transform.position - target.position;
     }
 
