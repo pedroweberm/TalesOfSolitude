5a37f71951b2b7809094ae449d383c2d4ae67215
Pedro Weber <pedroweberm@gmail.com>
2019-10-24 20:49:28 -0300

    Inicio da implementacao de interacao com arvores

---
+++ Assets/Local/Scripts/TreeController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+
+    public int treeHardChopTime = 5;
+    public int treeSoftChopTime = 2;
+
+    public int treeHardChopReward = 15;
+    public int treeSoftChopReward = 5;
+
+    public float interactableRadius = 3.0f;
+
+    public TickManager tickManager;
+
+    bool isBeingChopped = false;
+    int chopStartTick = 0;
+    int timeToChop = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (isBeingChopped)
+        {
+            if (tickManager.tickCount - chopStartTick >= timeToChop)
+            {
+                DestroyTree();
+            }
+        }
+    }
+
+    public void ChopTree(bool isHardChop)
+    {
+        chopStartTick = tickManager.tickCount;
+
+        if (isHardChop)
+        {
+            timeToChop = treeHardChopTime;
+        }
+        else
+        {
+            timeToChop = treeSoftChopTime;
+        }
+
+        isBeingChopped = true;
+    }
+
+    public void DestroyTree()
+    {
+        Destroy(this.gameObject);
+    }
+}
