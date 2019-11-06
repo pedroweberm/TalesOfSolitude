c90a47e1948c92b4cd7834410d721a9e1b6508a6
Gustavo Lorentz <gus.aca.lo@gmail.com>
2019-11-05 23:41:40 -0200

    Item script created

---
+++ Assets/Local/Scripts/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    public string item_name = "Item";
+    public Sprite icon = null;
+    public bool isResource = false;
+
+}
