8ec2f002db9e499435c3f1733afd072ee895bd02
Lorenzo <lopescernicchiaro@gmail.com>
2019-11-07 00:27:43 -0300

    starting

---
+++ Assets/Local/Scripts/ActionController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ActionController : MonoBehaviour
+{
+    public Button sampleButton;                         // sample button prefab
+    private List<ActionMenuItem> ActionMenuItems;     // list of items in menu
+
+    void Awake()
+    {
+        // Here we are creating and populating our future Action Menu.
+        // I do it in Awake once, but as you can see, 
+        // it can be edited at runtime anywhere and anytime.
+
+        ActionMenuItems = new List<ActionMenuItem>();
+        Action<Image> equip = new Action<Image>(EquipAction);
+        Action<Image> use = new Action<Image>(UseAction);
+        Action<Image> drop = new Action<Image>(DropAction);
+
+        ActionMenuItems.Add(new ActionMenuItem("Equip", sampleButton, equip));
+        ActionMenuItems.Add(new ActionMenuItem("Use", sampleButton, use));
+        ActionMenuItems.Add(new ActionMenuItem("Drop", sampleButton, drop));
+    }
+
+    void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+            ActionMenu.Instance.CreateActionMenu(ActionMenuItems, new Vector2(pos.x, pos.y));
+        }
+
+    }
+
+    void EquipAction(Image ActionPanel)
+    {
+        Debug.Log("Equipped");
+        Destroy(ActionPanel.gameObject);
+    }
+
+    void UseAction(Image ActionPanel)
+    {
+        Debug.Log("Used");
+        Destroy(ActionPanel.gameObject);
+    }
+
+    void DropAction(Image ActionPanel)
+    {
+        Debug.Log("Dropped");
+        Destroy(ActionPanel.gameObject);
+    }
+}
---
+++ Assets/Local/Scripts/ActionMenu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ActionMenuItem
+{
+    // this class - just a box to some data
+
+    public string text;             // text to display on button
+    public Button button;           // sample button prefab
+    public Action<Image> action;    // delegate to method that needs to be executed when button is clicked
+
+    public ActionMenuItem(string text, Button button, Action<Image> action)
+    {
+        this.text = text;
+        this.button = button;
+        this.action = action;
+    }
+}
+
+public class ActionMenu : MonoBehaviour
+{
+    public Image contentPanel;              // content panel prefab
+    public Canvas canvas;                   // link to main canvas, where will be Action Menu
+
+    private static ActionMenu instance;    // some kind of singleton here
+
+    public static ActionMenu Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType(typeof(ActionMenu)) as ActionMenu;
+                if (instance == null)
+                {
+                    instance = new ActionMenu();
+                }
+            }
+            return instance;
+        }
+    }
+
+    public void CreateActionMenu(List<ActionMenuItem> items, Vector2 position)
+    {
+        // here we are creating and displaying Action Menu
+
+        Image panel = Instantiate(contentPanel, new Vector3(position.x, position.y, 0), Quaternion.identity) as Image;
+        panel.transform.SetParent(canvas.transform);
+        panel.transform.SetAsLastSibling();
+        panel.rectTransform.anchoredPosition = position;
+
+        foreach (var item in items)
+        {
+            ActionMenuItem tempReference = item;
+            Button button = Instantiate(item.button) as Button;
+            Text buttonText = button.GetComponentInChildren(typeof(Text)) as Text;
+            buttonText.text = item.text;
+            button.onClick.AddListener(delegate { tempReference.action(panel); });
+            button.transform.SetParent(panel.transform);
+        }
+    }
+}
