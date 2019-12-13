b85134a6b07f063aff39b9ed250c83188bbf1823
Pedro Weber <pedroweberm@gmail.com>
2019-12-12 22:49:17 -0300

    added sound effects

---
+++ Assets/PlayerSounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Step()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+}
