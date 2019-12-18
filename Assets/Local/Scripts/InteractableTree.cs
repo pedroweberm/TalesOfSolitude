20028a3ad045967159fe0459f042e94e69665ef0
Pedro Weber <pedroweberm@gmail.com>
2019-12-18 11:13:21 -0300

    Interaction menu actions

--- Assets/Local/Scripts/InteractableTree.cs
+++ Assets/Local/Scripts/InteractableTree.cs
@@ -2,13 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(TreeController))]
 public class InteractableTree : Interactable
 {
     public GameObject actionMenu;
 
+    TreeController treeController;
+
+    private void Start()
+    {
+        treeController = GetComponent<TreeController>();
+    }
+
     public override void Interact()
     {
         base.Interact();
-        actionMenu.SetActive(true);
+    }
+
+    public override void openMenu()
+    {
+        base.openMenu();
+        //actionMenu.SetActive(true);
+    }
+
+    private void Update()
+    {
+        actionMenu.SetActive(isFocused);
+    }
+
+    public void SoftChop()
+    {
+        Debug.Log("soft");
+        PlayerController.instance.FollowFocus(this);
     }
 }
